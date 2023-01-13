fd91ad00a05d2d3d0caa2cc5b743e092b14c8f70
gthegryphon <52932380+gthegryphon@users.noreply.github.com>
2023-01-13 18:30:12 +0000

    implemented deck object for storing action card status between sessions

---
+++ src/main/java/dev/chambers/DAOs/DeckDAO.java
@@ -0,0 +1,83 @@
+package dev.chambers.DAOs;
+
+import dev.entities.Deck;
+
+public class DeckDAO{
+    public Deck getDeckByID(int sessionID){
+        try(Connection conn = ConnectionUtil.createConnection()){
+            String sql = "select * from decks where session_id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, session_id);            
+            ResultSet rs = ps.executeQuery;
+            if(rs.next()){
+                deck = new Deck(sessionID, rs.getString("decklist"));
+                return deck;
+            }else{return new Deck();}
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            return new Deck();
+        }
+    }
+    public Deck createDeck(){
+        String emptyDeck = "
+            0000000000
+            0000000000
+            0000000000
+            0000000000
+            0000000000
+            0";//51 digits, one per action card
+        try(Connection conn = ConnectionUtil.createConnection()){
+            String sql = "insert into decks values(default, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, emptyDeck);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                //need to fix to actually grab generated session id
+                Deck deck = new deck(rs.getInt("session_id"),emptyDeck);
+                return deck;
+                
+            }
+            else{return new Deck();}
+
+        }catch(SQLException e){
+            e.printStackTrace();
+            return new Deck();
+        }
+    }
+    public boolean saveDeck(Deck deck){
+        try(Connection conn = ConnectionUtil.createConnection()){
+            String sql = "update decks set decklist=? where session_id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,deck.deckList);
+            ps.setInt(2,deck.sessionID);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean deleteDeck(int sessionID){
+        try(Connection conn = ConnectionUtil.createConnection()){
+            String sql = "delete from decks where session_id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, sessionID);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/main/java/dev/chambers/entities/Deck.java
+++ src/main/java/dev/chambers/entities/Deck.java
@@ -1,6 +1,18 @@
 package dev.chambers.entities;
 
 public class Deck {
+    private int sessionID;
+    private String deckList;
+
+
+    public String getDeckList (int sessionID){}
+
+    public Deck(){}
+    public Deck(int sessionID, String deckList){
+        this.sessionID = sessionID;
+        this.deckList= deckList;
+    }
+
     //stretch
     //would track cards drawn/played during the session
 }
